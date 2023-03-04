335503884f8e465c5e7a05682fed0c427da56144
Konstantin Shibkov <shibkov.k@gmail.com>
2023-03-04 21:53:14 +0500

    add postgres

--- src/main/java/com/example/demo/spring/repository/MapStorage.java
+++ src/main/java/com/example/demo/spring/repository/MapStorage.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-@Repository
+
 public class MapStorage implements Storage{
 
     public final Map<Integer, String> vault=
